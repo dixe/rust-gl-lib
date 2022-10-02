935b05e69cdc86a62145c71d3e24fbc26936008f
dixe <dixe@github.com>
2022-10-02 16:30:27 +0200

    Added a depth map texture

--- src/texture.rs
+++ src/texture.rs
@@ -55,6 +55,34 @@ pub fn gen_texture_rgba(gl: &gl::Gl, img: &image::RgbaImage) -> TextureId {
 
 
 
+pub fn gen_texture_depth(gl: &gl::Gl, width: i32, height: i32) -> TextureId {
+
+    let mut id = 0;
+
+    unsafe {
+        gl.GenTextures(1, &mut id);
+
+        gl.BindTexture(gl::TEXTURE_2D, id);
+
+
+        gl.TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT as i32, width, height, 0, gl::DEPTH_COMPONENT, gl::FLOAT, 0 as *const gl::types::GLvoid);
+
+
+        gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+        gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
+
+
+        gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_BORDER as i32);
+        gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_BORDER as i32);
+
+        let border_colors : [f32; 4] = [1.0; 4];
+        gl.TexParameterfv(gl::TEXTURE_2D, gl::TEXTURE_BORDER_COLOR, border_colors.as_ptr() as *const gl::types::GLfloat) ;
+    }
+
+    id
+}
+
+
 /// Wrapper of BindTexture
 pub fn set_texture(gl: &gl::Gl, texture_id: TextureId) {
 
