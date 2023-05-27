72b579c468034c4e1602301ac8fd9adb58b59d2c
dixe <dixe@github.com>
2023-05-27 19:58:04 +0200

    Working subdivision into convex ploygons. But indices

--- examples/gjk/main.rs
+++ examples/gjk/main.rs
@@ -7,6 +7,9 @@ use std::collections::HashSet;
 type V2 = na::Vector2::<f32>;
 type V3 = na::Vector3::<f32>;
 
+mod polygon;
+use polygon::*;
+
 mod line_segment_intersection;
 use line_segment_intersection as lsi;
 
@@ -34,11 +37,11 @@ fn main() -> Result<(), failure::Error> {
     let mut state = State {
         polygon: Polygon {
             vertices: vec![],
-            sub_polygons: vec![]
         },
         selected: Default::default(),
         mode: Mode::NewPoint,
-        show_help: false
+        show_idx: false,
+        show_pos: false
     };
 
     loop {
@@ -62,7 +65,7 @@ fn main() -> Result<(), failure::Error> {
         ui.small_text("a select all, esc deslects");
 
 
-        render_poly(&mut ui, &mut state.polygon, &state.selected, state.show_help);
+        render_poly(&mut ui, &mut state.polygon, &state.selected, state.show_pos, state.show_idx);
 
         render_mode(&mut ui, &state.mode);
 
@@ -130,7 +133,7 @@ fn render_mode(ui: &mut Ui, mode: &Mode) {
 }
 
 
-fn render_poly(ui: &mut Ui, poly: &mut Polygon, selected: &HashSet::<usize>, show_help: bool) {
+fn render_poly(ui: &mut Ui, poly: &mut Polygon, selected: &HashSet::<usize>, show_pos: bool, show_idx: bool) {
 
     let len = poly.vertices.len();
 
@@ -143,21 +146,26 @@ fn render_poly(ui: &mut Ui, poly: &mut Polygon, selected: &HashSet::<usize>, sho
             r = 10.0;
         }
 
-        if show_help {
-            //ui.drawer2D.render_text(&format!("({:?}) - {i}", p1), p1.x as i32, p1.y as i32, 14);
-            ui.drawer2D.render_text(&format!("{i}"), p1.x as i32, p1.y as i32, 20);
-        }
 
         let mut drag = na::Vector2::<i32>::new(p1.x as i32, p1.y as i32);
         ui.drag_point(&mut drag, r);
         p1.x = drag.x as f32;
         p1.y = drag.y as f32;
 
+        if show_pos {
+            ui.drawer2D.render_text(&format!("({:?})", p1), p1.x as i32, p1.y as i32 + 20, 14);
+        }
+
+        if show_idx {
+            ui.drawer2D.render_text(&format!("{i}"), p1.x as i32, p1.y as i32, 14);
+        }
+
         if i < len - 1 {
             let p1 = poly.vertices[i];
             let p2 = poly.vertices[i + 1];
             ui.drawer2D.line(p1.x, p1.y, p2.x, p2.y, 3.0);
         }
+
     }
 
     if len > 2 {
@@ -165,13 +173,13 @@ fn render_poly(ui: &mut Ui, poly: &mut Polygon, selected: &HashSet::<usize>, sho
         let p2 = poly.vertices[0];
         ui.drawer2D.line(p1.x, p1.y, p2.x, p2.y, 3.0);
     }
-
+/*
     for sub_p in &poly.sub_polygons {
         for idx in &sub_p.indices {
             ui.drawer2D.rounded_rect(poly.vertices[*idx].x, poly.vertices[*idx].y, 10, 10);
         }
     };
-
+*/
     render_intersect(ui, poly);
 }
 
@@ -179,7 +187,8 @@ struct State {
     polygon: Polygon,
     selected: HashSet::<usize>,
     mode: Mode,
-    show_help: bool
+    show_idx: bool,
+    show_pos: bool
 }
 
 
@@ -187,17 +196,6 @@ pub enum Mode {
     NewPoint,
     Select(V2, V2),
 }
-
-pub struct Polygon {
-    vertices: Vec::<V2>,
-    sub_polygons: Vec::<SubPolygon>
-}
-
-
-pub struct SubPolygon {
-    indices: Vec::<usize>
-}
-
 
 fn handle_inputs(ui: &mut Ui, state: &mut State) {
 
@@ -263,18 +261,22 @@ fn handle_inputs(ui: &mut Ui, state: &mut State) {
             },
 
             KeyDown { keycode: Some(Keycode::S), ..} => {
+                println!("\n\n");
                 calulate_subdivision(&mut state.polygon);
             },
 
             KeyDown { keycode: Some(Keycode::Tab), ..} => {
-                state.show_help = !state.show_help
+                state.show_idx = !state.show_idx
+            },
+
+            KeyDown { keycode: Some(Keycode::Backquote), ..} => {
+                state.show_pos = !state.show_pos
             },
 
             KeyDown { keycode: Some(Keycode::C), ..} => {
                 state.selected.clear();
                  state.polygon = Polygon {
                      vertices: vec![],
-                     sub_polygons: vec![]
                  };
             },
 
@@ -300,268 +302,4 @@ fn handle_inputs(ui: &mut Ui, state: &mut State) {
 
         }
     }
-}
-
-
-
-
-fn calulate_subdivision(polygon: &mut Polygon) {
-    polygon.sub_polygons.clear();
-
-    // first make it ofirst make
-    polygon.sub_polygons.push(SubPolygon {
-        indices: vec![]
-    });
-
-
-    let dir = direction(&polygon);
-
-    match dir {
-        Dir::Left => {
-            polygon.vertices.reverse();
-        },
-        _ => {
-        }
-    }
-
-    if let Some(wide_idx) = first_wide(polygon) {
-        // search from left to right for the first polygon that does not produce any intersections
-        // and does not make a line outside the polygon
-
-        // loop over posible connections
-
-        println!("Found: {:?}", find_valid_connection(polygon, wide_idx));
-    }
-}
-
-fn find_valid_connection(polygon: &Polygon, idx: usize) -> usize {
-    let cur_p = polygon.vertices[idx];
-
-    let len = polygon.vertices.len();
-
-    let mut res = 0;
-    for conn_idx in 0..len {
-        let mut valid = true;
-
-        if conn_idx == (len + idx - 1) % len || conn_idx == idx || conn_idx == (idx + 1 % len) {
-            // skip0 neighbours and same index
-            continue;
-        }
-        let connection_point = polygon.vertices[conn_idx];
-
-        // now check all lines in polygon
-
-        'inner: for i in 0..len {
-            let idx1 = i;
-            let idx2 = (i + 1) % len;
-            if idx1 == idx || idx1 == conn_idx
-                || idx2 == idx || idx2 == conn_idx {
-                    continue;
-                }
-            let c = polygon.vertices[idx1];
-            let d = polygon.vertices[idx2];
-
-            if let Some(_) = lsi::line_segment_intersect(cur_p, connection_point, c,d) {
-                // line conn
-                valid = false;
-                break 'inner;
-            }
-        }
-
-
-        if valid {
-            let inside = is_inside(idx, conn_idx, polygon);
-            res = conn_idx;
-            println!("Found {:?}", (idx, conn_idx, inside));
-        }
-    }
-
-    res
-}
-
-fn is_inside(idx1: usize, idx2: usize, polygon: &Polygon) -> bool {
-
-    // just check that the new edge is between the two edges.
-    // find incoming edge as normalization
-    // new edge is higher angle than basline,
-    // new edge is lower angle than outgoing edge
-
-    let len = polygon.vertices.len();
-    let from = polygon.vertices[(len + idx1 - 1) % len];
-    let p = polygon.vertices[idx1];
-    let to = polygon.vertices[(idx1 + 1) % len];
-
-    let mut from_dir = from - p;
-    from_dir.y *= -1.0; // sdl inverse coords, neeed to be fixed when using atan2
-    let mut to_dir = to - p;
-    to_dir.y *= -1.0;
-
-    // between -pi and pi has to be in 0 to tau
-    let v1 = from_dir.y.atan2(from_dir.x);
-    let mut v2 = to_dir.y.atan2(to_dir.x);
-    if v2 < 0.0 {
-        v2 += std::f32::consts::TAU;
-    }
-    v2 -= v1;
-
-    let mut new_dir = polygon.vertices[idx2] - polygon.vertices[idx1];
-    new_dir.y *= -1.0;
-
-    let mut new_v = (new_dir.y).atan2(new_dir.x);
-    if new_v < 0.0 {
-        new_v += std::f32::consts::TAU;
-    }
-
-    new_v -= v1;
-
-
-
-    return new_v < v2 && new_v > 0.0;
-}
-
-
-fn first_wide(polygon: &Polygon) -> Option<usize> {
-    let len = polygon.vertices.len();
-
-    for i in 0..polygon.vertices.len() {
-        let before = polygon.vertices[(len + i - 1) % len];
-        let pi = polygon.vertices[i];
-        let after = polygon.vertices[(i + 1) % len];
-
-        if is_wide_angle(vec3(before), vec3(pi), vec3(after)) {
-            return Some(i);
-        }
-    }
-
-    None
-}
-
-fn wide_indices(polygon: &Polygon) -> Vec::<usize> {
-    let mut res = vec![];
-
-    let len = polygon.vertices.len();
-
-    for i in 0..polygon.vertices.len() {
-        let before = polygon.vertices[(len + i - 1) % len];
-        let pi = polygon.vertices[i];
-        let after = polygon.vertices[(i + 1) % len];
-
-        if is_wide_angle(vec3(before), vec3(pi), vec3(after)) {
-            res.push(i);
-        }
-    }
-
-    res
-
-}
-
-fn calulate_subdivision_2(polygon: &mut Polygon) {
-
-    let len = polygon.vertices.len();
-    let mut min_dot = 1.0;
-    let mut min_idx = 0;
-    println!("\n\n\n");
-    for i in 0..polygon.vertices.len() {
-        let before = polygon.vertices[(len + i - 1) % len];
-        let pi = polygon.vertices[i];
-        let after = polygon.vertices[(i + 1) % len];
-
-
-
-        // Find v1 that is how much to rotation dir1 to align with x axis
-
-        let mut dir1 = (before - pi).normalize();
-        // invert the y, sdl has 0 at top and max at bottom, atan2 expect the reverse for y
-        dir1.y *= -1.0;
-
-        let v1 = dir1.y.atan2(dir1.x);
-
-        let mut dir2 = (after - pi).normalize();
-        // invert the y, sdl has 0 at top and max at bottom, atan2 expect the reverse for y
-        dir2.y *= -1.0;
-
-        let mut v2 = dir2.y.atan2(dir2.x);
-        if v2 < 0.0 {
-            v2 += std::f32::consts::TAU;
-        }
-
-        // v1 is negative angle, that is how much to rotate to hav that align with x axis, add (subtract) from v2
-        let mut v = v2 -v1;
-        if v > std::f32::consts::TAU {
-            v -= std::f32::consts::TAU;
-        }
-        println!("{:?}", v);
-        let d = dir1.dot(&dir2);
-        if d <= 0.0 {
-            //polygon.sub_polygons[0].indices.push(i);
-        }
-
-        if is_wide_angle(vec3(before), vec3(pi), vec3(after)) {
-            polygon.sub_polygons[0].indices.push(i);
-        }
-
-    }
-
-}
-
-
-
-fn direction(polygon: &Polygon) -> Dir {
-
-    let mut num_wide = 0;
-
-    // assume right, and if not return left
-
-    for i in 1..polygon.vertices.len() {
-        let v1_i = (i + 1) % polygon.vertices.len();
-        let v2_i = (i + 2) % polygon.vertices.len();
-
-        let v0 = vec3(polygon.vertices[i]);
-        let v1 = vec3(polygon.vertices[v1_i]);
-        let v2 = vec3(polygon.vertices[v2_i]);
-
-        if is_wide_angle(v0, v1, v2) {
-            num_wide += 1;
-        }
-    }
-
-
-    if num_wide > (polygon.vertices.len()  / 2 ) {
-        return Dir::Left;
-    }
-
-    return Dir::Right;
-}
-
-fn vec3(v: V2) -> V3{
-    V3::new(v.x, v.y, 0.0)
-}
-
-// The triangles are always right handed. So when the cross product is below 0 between the two edges the angle is > 180 deg
-fn is_wide_angle(v0: na::Vector3::<f32>, v1: na::Vector3::<f32>, v2: na::Vector3::<f32>) -> bool {
-    (v1 - v0).cross(&(v2-v1)).z < 0.0
-}
-
-
-enum Dir {
-    Left,
-    Right
-}
-
-
-
-
-
-fn tmp() {
-
-    /*
-    polygon_shader.shader.set_used();
-
-
-    let transform = na::Matrix4::<f32>::identity();
-
-    polygon_shader.shader.set_mat4(gl, "transform", transform);
-
-    polygon.render(gl);
-     */
 }
---
+++ examples/gjk/polygon.rs
@@ -0,0 +1,283 @@
+use super::*;
+
+#[derive(Debug)]
+pub struct Polygon {
+    pub vertices: Vec::<V2>
+}
+
+#[derive(Debug)]
+pub struct SubPolygon<'a> {
+    pub polygon: &'a Polygon,
+    pub indices: Vec::<usize>,
+}
+
+impl<'a> SubPolygon<'a> {
+
+    /// assume i in [0;len]
+    pub fn vertex(&self, i: usize) -> V2 {
+        let idx = self.indices[i];
+        self.polygon.vertices[idx]
+    }
+
+    pub fn len(&self) -> usize {
+        self.indices.len()
+    }
+
+}
+
+pub fn calulate_subdivision(polygon: &mut Polygon) -> Vec::<SubPolygon> {
+    let dir = direction(&polygon);
+    match dir {
+        Dir::Left => {
+            polygon.vertices.reverse();
+        },
+        _ => {
+        }
+    }
+
+    let mut input_sub_p = SubPolygon {
+        polygon,
+        indices: vec![]
+    };
+
+    for i in 0..polygon.vertices.len() {
+        input_sub_p.indices.push(i);
+    }
+
+    let mut to_be_checked = vec![input_sub_p];
+
+    let mut res = vec![];
+    while let Some(sub_p) = to_be_checked.pop() {
+        // search from left to right for the first polygon that does not produce any intersections
+        // and does not make a line outside the polygon
+
+        if let Some(wide_idx) = first_wide(&sub_p) {
+            let connection = find_valid_connection(&sub_p, wide_idx);
+
+            // connection and wide_idx is indices into indices of sub_p
+            let(s1,s2) = split_polygon(sub_p, wide_idx, connection);
+            to_be_checked.push(s1);
+            to_be_checked.push(s2);
+        } else {
+            println!("{:?}", sub_p.indices);
+            res.push(sub_p);
+        }
+    }
+
+    res
+}
+
+fn split_polygon(sub_p: SubPolygon, from: usize, to: usize) -> (SubPolygon, SubPolygon) {
+
+    // connection and wide_idx is indices into indices of sub_p
+
+    // first ploygon is from 0 to min, and from max to max
+    // second is from min to max
+
+    let mut s1 = SubPolygon {
+        polygon: sub_p.polygon,
+        indices: vec![]
+    };
+
+
+    // for s1 take all indices in sub_p.indices from 0..=from then take all in to..sub_p.indices.len
+
+    // for s0sub_p.indices from 0..=from then take all in to..sub_p.indices.len
+
+
+    for i in 0..=from {
+        s1.indices.push(sub_p.indices[i]);
+    }
+
+    if to > from {
+        for i in to..sub_p.indices.len() {
+            s1.indices.push(sub_p.indices[i]);
+        }
+    }
+
+    let mut s2 = SubPolygon {
+        polygon: sub_p.polygon,
+        indices: vec![]
+    };
+
+
+    if to > from {
+        for i in from..=to {
+            s2.indices.push(sub_p.indices[i]);
+        }
+    } else {
+        let len = sub_p.len();
+        for i in from..=(to + len) {
+            s2.indices.push(sub_p.indices[i % len]);
+        }
+    }
+
+    (s1, s2)
+}
+
+fn find_valid_connection(sub_p: &SubPolygon, idx: usize) -> usize {
+    let cur_p = sub_p.vertex(idx);
+
+    let len = sub_p.len();
+
+    let mut res = 0;
+    for conn_idx in 0..len {
+        let mut valid = true;
+        if conn_idx == (len + idx - 1) % len || conn_idx == idx || conn_idx == (idx + 1) % len {
+            // skip0 neighbours and same index
+            continue;
+        }
+
+        let connection_point = sub_p.vertex(conn_idx);
+
+        // now check all lines in polygon
+        'inner: for i in 0..len {
+            let idx1 = i;
+            let idx2 = (i + 1) % len;
+            if idx1 == idx || idx1 == conn_idx
+                || idx2 == idx || idx2 == conn_idx {
+                    continue;
+                }
+            let c = sub_p.vertex(idx1);
+            let d = sub_p.vertex(idx2);
+
+            if let Some(_) = lsi::line_segment_intersect(cur_p, connection_point, c,d) {
+                // line conn
+                valid = false;
+                break 'inner;
+            }
+        }
+
+
+        if valid && is_inside(idx, conn_idx, &sub_p) {
+            res = conn_idx;
+        }
+    }
+
+    res
+}
+
+fn is_inside(idx1: usize, idx2: usize, sub_p: &SubPolygon) -> bool {
+
+    // just check that the new edge is between the two edges.
+    // find incoming edge as normalization
+    // new edge is higher angle than basline,
+    // new edge is lower angle than outgoing edge
+
+    let len = sub_p.len();
+    let from = sub_p.vertex((len + idx1 - 1) % len);
+    let p = sub_p.vertex(idx1);
+    let to = sub_p.vertex((idx1 + 1) % len);
+
+    let mut from_dir = from - p;
+    from_dir.y *= -1.0; // sdl inverse coords, neeed to be fixed when using atan2
+    let mut to_dir = to - p;
+    to_dir.y *= -1.0;
+
+    // between -pi and pi has to be in 0 to tau
+    let v1 = from_dir.y.atan2(from_dir.x);
+    let mut v2 = to_dir.y.atan2(to_dir.x);
+    if v2 < 0.0 {
+        v2 += std::f32::consts::TAU;
+    }
+    v2 -= v1;
+
+    if v2 < 0.0 {
+        v2 += std::f32::consts::TAU;
+    }
+
+    let mut new_dir = sub_p.vertex(idx2) - sub_p.vertex(idx1);
+    new_dir.y *= -1.0;
+
+    let mut new_v = (new_dir.y).atan2(new_dir.x);
+    if new_v < 0.0 {
+        new_v += std::f32::consts::TAU;
+    }
+
+    new_v -= v1;
+
+    if new_v < 0.0 {
+        new_v += std::f32::consts::TAU;
+    }
+
+    return new_v < v2 && new_v > 0.0;
+}
+
+
+fn first_wide(sub_p: &SubPolygon) -> Option<usize> {
+    let len = sub_p.indices.len();
+
+    for i in 0..len {
+        let before = sub_p.vertex((len + i - 1) % len);
+        let pi = sub_p.vertex(i);
+        let after = sub_p.vertex((i + 1) % len);
+
+        if is_wide_angle(vec3(before), vec3(pi), vec3(after)) {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn wide_indices(polygon: &Polygon) -> Vec::<usize> {
+    let mut res = vec![];
+
+    let len = polygon.vertices.len();
+
+    for i in 0..polygon.vertices.len() {
+        let before = polygon.vertices[(len + i - 1) % len];
+        let pi = polygon.vertices[i];
+        let after = polygon.vertices[(i + 1) % len];
+
+        if is_wide_angle(vec3(before), vec3(pi), vec3(after)) {
+            res.push(i);
+        }
+    }
+
+    res
+
+}
+
+
+fn direction(polygon: &Polygon) -> Dir {
+
+    let mut num_wide = 0;
+
+    // assume right, and if not return left
+
+    for i in 1..polygon.vertices.len() {
+        let v1_i = (i + 1) % polygon.vertices.len();
+        let v2_i = (i + 2) % polygon.vertices.len();
+
+        let v0 = vec3(polygon.vertices[i]);
+        let v1 = vec3(polygon.vertices[v1_i]);
+        let v2 = vec3(polygon.vertices[v2_i]);
+
+        if is_wide_angle(v0, v1, v2) {
+            num_wide += 1;
+        }
+    }
+
+
+    if num_wide > (polygon.vertices.len()  / 2 ) {
+        return Dir::Left;
+    }
+
+    return Dir::Right;
+}
+
+fn vec3(v: V2) -> V3{
+    V3::new(v.x, v.y, 0.0)
+}
+
+// The triangles are always right handed. So when the cross product is below 0 between the two edges the angle is > 180 deg
+fn is_wide_angle(v0: na::Vector3::<f32>, v1: na::Vector3::<f32>, v2: na::Vector3::<f32>) -> bool {
+    (v1 - v0).cross(&(v2-v1)).z < 0.0
+}
+
+
+enum Dir {
+    Left,
+    Right
+}
